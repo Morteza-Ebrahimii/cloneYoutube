5d6d272a67a3e4e2e3d9c0b790142af567e39990
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-12-17 13:09:19 +0330

    first commit

---
+++ clone-youtube/src/App.tsx
@@ -0,0 +1,14 @@
+import { PageHeader } from "./layout/PageHeader"
+
+
+function App() {
+
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div>2</div>
+    </div>
+  )
+}
+
+export default App
