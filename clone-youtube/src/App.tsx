b6a5c68c754de54ff5842eebfcae5ee015f05a31
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-12-19 11:42:34 +0330

    feat: add categories; hard code categories

--- clone-youtube/src/App.tsx
+++ clone-youtube/src/App.tsx
@@ -1,14 +1,19 @@
-import { PageHeader } from "./layout/PageHeader"
-
+import { CategoryPills } from "./component/CategoryPills";
+import { categories } from "./data/home";
+import { PageHeader } from "./layout/PageHeader";
 
 function App() {
-
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div>2</div>
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidbar</div>
+        <div className="sticky top-0 bg-white z-10 pb-4">
+          <CategoryPills categories={categories} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
