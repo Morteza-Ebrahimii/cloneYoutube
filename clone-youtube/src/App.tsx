5c28615443acd73f1f82fa28039aaa3a854b59a2
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-12-20 13:10:06 +0330

    handle sidbar header

--- clone-youtube/src/App.tsx
+++ clone-youtube/src/App.tsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import { CategoryPills } from "./component/CategoryPills";
 import { categories } from "./data/home";
 import { PageHeader } from "./layout/PageHeader";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
         <div>Sidbar</div>
-        <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills categories={categories} />
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
         </div>
       </div>
     </div>
