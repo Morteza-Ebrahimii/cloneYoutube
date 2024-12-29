d74882832a0cdd962980615e2447e62f4e1967bc
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-12-29 12:40:19 +0330

    feat: handle sidbar section

--- clone-youtube/src/App.tsx
+++ clone-youtube/src/App.tsx
@@ -3,6 +3,7 @@ import { CategoryPills } from "./component/CategoryPills";
 import { categories, videos } from "./data/home";
 import { PageHeader } from "./layout/PageHeader";
 import { VideoGridItem } from "./component/VideoGridItem";
+import { Sidebar } from "./layout/Sidebar";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -11,7 +12,7 @@ function App() {
     <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <div>Sidbar</div>
+        <Sidebar />
         <div className="overflow-x-hidden px-8 pb-4">
           <div className="sticky top-0 bg-white z-10 pb-4">
             <CategoryPills
